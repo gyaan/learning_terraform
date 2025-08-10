71b4987ba3c3a60a18b412b5c42f05764bd5b909
gyaan <gyaan1334@gmail.com>
2025-08-10 16:13:51 +0530

    Fix typo in security group reference for web instance in main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = aws_security_groups.web.id
+  vpc_security_group_ids = aws_security_group.web.id
   tags = {
     Name = "HelloWorld"
   }
